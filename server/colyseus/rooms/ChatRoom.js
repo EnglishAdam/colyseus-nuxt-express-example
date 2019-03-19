ea8a9d367b80e568938a2c8d7e69b5c91c393fa0
Adam English <englishadam@hotmail.co.uk>
2019-03-19 23:08:15 +0000

    Migrated rooms to own folder

---
+++ server/colyseus/rooms/ChatRoom.js
@@ -0,0 +1,41 @@
+const colyseus = require('colyseus')
+const consola = require('consola')
+const Room = colyseus.Room
+
+class ChatRoom extends Room {
+    constructor() {
+        super()
+        // this room supports only 4 clients connected
+        this.maxClients = 4;
+    }
+
+    onInit (options) {
+        consola.log("BasicRoom created!", options);
+    }
+
+    onJoin (client) {
+        this.broadcast(`${ client.sessionId } joined.`);
+    }
+
+    onLeave (client) {
+        this.broadcast(`${ client.sessionId } left.`);
+    }
+
+    onMessage (client, data) {
+        consola.log("BasicRoom received message from", client.sessionId, ":", data);
+        this.broadcast(`(${ client.sessionId }) ${ data.message }`);
+    }
+
+    onDispose () {
+        consola.log("Dispose BasicRoom");
+    }
+
+}
+
+module.exports = {
+    name: 'chat',
+    room: ChatRoom,
+    options: {
+        test: 'testOption'
+    }
+}
---
+++ server/colyseus/rooms/rooms.js
@@ -0,0 +1,6 @@
+/**
+ * A list of chat rooms and their name
+ */
+module.exports = [
+  require('./ChatRoom')
+]
